15b641797d846f861edd2d1d1ee093b925b4d031
Muneeb <freelancermuneeb@gmail.com>
2024-08-20 19:53:16 +0500

    add basic modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  devServer: {
+    port: 3000,
+    host: '127.0.0.1'
+  },
+
+  modules: [
+    '@pinia/nuxt',
+    '@formkit/auto-animate',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxtjs/google-fonts',
+    '@pinia-plugin-persistedstate/nuxt',
+    'shadcn-nuxt',
+    'nuxt-headlessui'
+  ]
 })
